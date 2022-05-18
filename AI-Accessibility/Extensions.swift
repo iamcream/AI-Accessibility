5e722cf9ba333b9880440f0fb2ddaf36771b487c
iamcream <lypomelo@connect.hku.hk>
2022-05-18 16:03:58 +0800

    color and edge

--- AI-Accessibility/Extensions.swift
+++ AI-Accessibility/Extensions.swift
@@ -14,8 +14,22 @@ extension UIView {
                             green: CGFloat(pixel[1] / 255),
                             blue: CGFloat(pixel[2]) / 255,
                             alpha: CGFloat(pixel[3]) / 255)
-        print("pixel:",pixel)
+        pixel.deallocate()
+        return color
+    }
+    
+    func edgeOfTouch (point: CGPoint) -> UIColor {
+        let pixel = UnsafeMutablePointer<CUnsignedChar>.allocate(capacity: 4)
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedLast.rawValue)
+        let context = CGContext(data: pixel, width: 1, height: 1, bitsPerComponent: 8, bytesPerRow: 4, space: colorSpace, bitmapInfo: bitmapInfo.rawValue)!
         
+        context.translateBy(x: -point.x, y: -point.y)
+        //self.layer.render(in: context)
+        let color = UIColor(red: CGFloat(pixel[0]) / 255,
+                            green: CGFloat(pixel[1] / 255),
+                            blue: CGFloat(pixel[2]) / 255,
+                            alpha: CGFloat(pixel[3]) / 255)
         pixel.deallocate()
         return color
     }
@@ -26,7 +40,6 @@ extension UIColor {
         guard let components = cgColor.components, components.count >= 3 else {
             return ""
         }
-        
         let r = Float(components[0]) * 255
         let g = Float(components[1]) * 255
         let b = Float(components[2]) * 255
@@ -37,18 +50,86 @@ extension UIColor {
                           lroundf(b))
     }
     
+    
     func rgb() -> String {
+            guard let components = cgColor.components, components.count >= 3 else {
+                return ""
+            }
+            
+            let r = Float(components[0]) * 255
+            let g = Float(components[1]) * 255
+            let b = Float(components[2]) * 255
+
+            return String(format: "R: %d G: %d B: %d",
+                              lroundf(r),
+                              lroundf(g),
+                              lroundf(b))
+        }
+    
+    func rgbFloat() -> (r: Float,g: Float,b: Float) {
         guard let components = cgColor.components, components.count >= 3 else {
-            return ""
+            return (-1.0,-1.0,-1.0)
         }
         
-        let r = Float(components[0]) * 255
-        let g = Float(components[1]) * 255
-        let b = Float(components[2]) * 255
-
-        return String(format: "R: %d G: %d B: %d",
+        let r = Float(components[0])
+        let g = Float(components[1])
+        let b = Float(components[2])
+        
+        /*return String(format: "R: %d G: %d B: %d",
                           lroundf(r),
                           lroundf(g),
                           lroundf(b))
+         */
+        return (r,g,b)
+         
     }
+
+    
+}
+
+extension UIImage{
+    func getPixelColor(pos: CGPoint) -> UIColor {
+        let pixelData = self.cgImage!.dataProvider!.data
+        //let pixelData = CGDataProviderCopyData(CGImageGetDataProvider(self.cgImage))
+        let data: UnsafePointer<UInt8> = CFDataGetBytePtr(pixelData)
+        print("pos:",pos)
+        print("x:",pos.x,"y:",pos.y)
+        print("size:",self.size)
+        let pixelInfo: Int = ((Int(self.size.width) * Int(pos.y)) + Int(pos.x)) * 4
+        let r = CGFloat(data[pixelInfo]) / CGFloat(255.0)
+        let g = CGFloat(data[pixelInfo+1]) / CGFloat(255.0)
+        let b = CGFloat(data[pixelInfo+2]) / CGFloat(255.0)
+        let a = CGFloat(data[pixelInfo+3]) / CGFloat(255.0)
+    
+        return UIColor(red: r, green: g, blue: b, alpha: a)
+    }
+    
+    /*
+    func cxg_getPointColor(withImage image: UIImage, point: CGPoint) -> UIColor {
+
+        let pointX = trunc(point.x);
+        let pointY = trunc(point.y);
+
+        let width = image.size.width;
+        let height = image.size.height;
+        let colorSpace = CGColorSpaceCreateDeviceRGB();
+        var pixelData: [UInt8] = [0, 0, 0, 0]
+
+        pixelData.withUnsafeMutableBytes { pointer in
+            if let context = CGContext(data: pointer.baseAddress, width: 1, height: 1, bitsPerComponent: 8, bytesPerRow: 4, space: colorSpace, bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue), let cgImage = image.cgImage {
+                context.setBlendMode(.copy)
+                context.translateBy(x: -pointX, y: pointY - height)
+                context.draw(cgImage, in: CGRect(x: 0, y: 0, width: width, height: height))
+            }
+        }
+
+        let red = CGFloat(pixelData[0]) / CGFloat(255.0)
+        let green = CGFloat(pixelData[1]) / CGFloat(255.0)
+        let blue = CGFloat(pixelData[2]) / CGFloat(255.0)
+        let alpha = CGFloat(pixelData[3]) / CGFloat(255.0)
+
+        return UIColor(red: red, green: green, blue: blue, alpha: alpha)
+    }
+    */
+   
 }
--- AI-Accessibility/ImageTouchViewController.swift
+++ AI-Accessibility/ImageTouchViewController.swift
@@ -2,7 +2,7 @@ import UIKit
 import Alamofire
 import SwiftyJSON
 import AVFoundation
-
+import GPUImage
 
 class ImageTouchViewController: UIViewController {
 
@@ -10,10 +10,13 @@ class ImageTouchViewController: UIViewController {
     let apiKey = "602d63362348496ea03ae6a766b4527a"
     let apiEndPoint = "https://accessibility-ai-ly.cognitiveservices.azure.com/vision/v3.2/analyze?visualFeatures=Description" 
     
+    var desc: String!
+    var color_desc: String!
     let syntesizer = AVSpeechSynthesizer()
     var utterance = AVSpeechUtterance()
     
     var selectImage: UIImage
+    var filteredImage: UIImage!
     
     init?(coder: NSCoder,passImage: UIImage){
         self.selectImage = passImage
@@ -27,7 +30,7 @@ class ImageTouchViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         imageTouchView.image = selectImage
-        
+        edgeDetection()
         //tap gesture
         //let tap = UITapGestureRecognizer(target: self, action: #selector(tapView(tap:)))
         imageTouchView.isUserInteractionEnabled = true
@@ -35,12 +38,18 @@ class ImageTouchViewController: UIViewController {
         ImageUpload(self.selectImage)
         // Do any additional setup after loading the view.
         
+        
+    }
+    
+    @IBAction func playAgain(_ sender: Any) {
+        self.audio(text: self.desc)
     }
     
     override var prefersStatusBarHidden: Bool {
             return true
         }
     
+    //audio description
     func ImageUpload(_ image: UIImage) {
     guard image.jpegData(compressionQuality: 0.9) != nil else {
                 self.dismiss(animated: true, completion: nil)
@@ -65,8 +74,9 @@ class ImageTouchViewController: UIViewController {
                         let des = json["description"] as! [String: Any]
                         let cap = des["captions"] as! [[String: Any]]
                         let destext = cap[0]["text"] as! String
-                        print(destext)
-                        self.audio(text: destext)
+                        self.desc = destext
+                        print(self.desc!)
+                        self.audio(text: self.desc)
                     }
                 case .failure(let encodingError):
                     print("ERROR RESPONSE: \(encodingError)")
@@ -77,22 +87,173 @@ class ImageTouchViewController: UIViewController {
     func audio(text: String) {
         utterance = AVSpeechUtterance (string: text)
             // 语音的速度
-            utterance.rate = 0.4
+            utterance.rate = 0.3
             // 开始朗读
             syntesizer.speak(utterance)
         }
-    /*
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+ 
         if let touch = touches.first {
+            //edge
+            edgeDetection()
+            //color
+            /*
             let tapPoint = touch.location(in: self.view)
             let loc = tapPoint
             let color = imageTouchView.colorOfTouch(point: loc)
-
             print(color.rgb())
-            print(color.hex())
+             */
+            let tapPoint = touch.location(in: self.view)
+            let loc = tapPoint
+            
+            let color = self.selectImage.getPixelColor(pos: loc)
+            //let color = imageTouchView.colorOfTouch(point: loc)
+            print("color:",color)
+            print("color_in_255:",color.rgb())
+            let color_array = color.rgbFloat()
+            self.color_desc = recogColor(r: color_array.0, g: color_array.1, b: color_array.2)
+            print("color_desc:",self.color_desc!)
+            self.audio(text: self.color_desc)
+            
+            let edge_color = self.filteredImage.getPixelColor(pos: loc)
+            print("edge_color:",edge_color)
+            print("edge_color_in_255:",edge_color.rgb())
+            let edge_color_array = edge_color.rgbFloat()
+            print(edge_color_array)
+            if(edge_color_array.0 >= 0.6){
+                self.audio(text: "edge")
+            }
+            
+            //print(color.rgb())
+            //print("edge:",edge_color)
+            
+            
         }
     }
-
+    
+    func recogColor(r: Float, g: Float, b: Float) -> String{
+        let rgb_array = [r,g,b]
+        print("r-g:",r-g)
+        print("r-b:",r-b)
+        print("g-b:",g-b)
+        var final_color: String!
+        var color_threshold: Float
+        var color_threshold2: Float
+        color_threshold = 0.15
+        color_threshold2 = 0.05
+        if (r == 1.0 && g == 1.0 && b == 1.0){
+            final_color = "White"
+        }else if (r == 0.0 && g == 0.0 && b == 0.0){
+                final_color = "Black"
+        }else if (abs(r-g) <= color_threshold2 && abs(r-b) <= color_threshold2 && abs(g-b) <= color_threshold2){
+            final_color = "Gray"
+        }else if (rgb_array.max() == r){
+            /*
+            if(r-g <= color_threshold && r-b >= color_threshold){
+                final_color = "Yellow"
+            }
+            else if(r-g >= color_threshold && r-b <= color_threshold){
+                final_color = "Pink"
+            }
+            else{
+                final_color = "Red"
+            }
+            */
+            if (g >= b){
+                if (r-g <= color_threshold){
+                    final_color = "Yellow"
+                }
+                else{
+                    final_color = "Red"
+                }
+            }
+            if (b > g){
+                if (r-b <= color_threshold){
+                    final_color = "Pink"
+                }
+                else{
+                    final_color = "Red"
+                }
+            }
+            
+        }else if (rgb_array.max() == g){
+            if (r >= b){
+                if(g-r <= color_threshold){
+                    final_color = "Yellow"
+                }
+                else{
+                    final_color = "Green"
+                }
+            }
+            if(b > r){
+                if(g-b <= color_threshold){
+                    final_color = "Cyan"
+                }
+                else{
+                    final_color = "Green"
+                }
+            }
+            
+            /*
+            if(g-r <= color_threshold && g-b >= color_threshold){
+                final_color = "Yellow"
+            }
+            else if(g-r >= color_threshold && g-b <= color_threshold){
+                final_color = "Cyan"
+            }
+            else{
+                final_color = "Green"
+            }
+             */
+        }else if (rgb_array.max() == b){
+            if(r >= g){
+                if(b-r <= color_threshold){
+                    final_color = "Pink"
+                }
+                else{
+                    final_color = "Blue"
+                }
+            }
+            else if(g > r){
+                if(b-g <= color_threshold){
+                    final_color = "Cyan"
+                }
+                else{
+                    final_color = "Blue"
+                }
+            }
+            
+            /*
+            if(b-r <= color_threshold && b-g >= color_threshold){
+                final_color = "Pink"
+            }
+            else if(b-r >= color_threshold && b-g <= color_threshold){
+                final_color = "Cyan"
+            }
+            else{
+                final_color = "Blue"
+            }
+             */
+        }
+            return final_color
+    }
+    
+    //edge detection
+    func edgeDetection(){
+        let inputImage = self.selectImage
+        let filter = SobelEdgeDetection()
+        filter.edgeStrength = 3
+        //let filter = CannyEdgeDetection()
+        //filter.lowerThreshold = 0.3
+        //filter.upperThreshold = 0.5
+        let filteredImage = inputImage.filterWithOperation(filter)
+        self.filteredImage = filteredImage
+        //self.imageTouchView.image = filteredImage
+    }
+    
+    
+    /*
     @objc func tapView(tap: UITapGestureRecognizer) {
         print("single tap")
         
